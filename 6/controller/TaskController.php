ba2b59546616871fead81d84d984337279d3c87f
Nikolay Kutepov <nkutepov@hotmail.com>
2023-02-14 17:48:50 +0300

    home-task-6-some-changings

--- 6/controller/TaskController.php
+++ 6/controller/TaskController.php
@@ -16,16 +16,22 @@ if (isset($_SESSION['user']) &&  $name) {
 
 
     if (isset($_POST['oneMore']) && !empty($_POST['oneMore'])) {
-        setcookie('list', TaskProvider::addTask($_POST['oneMore'], $list));
-        header('Location:?controller=todo');
+        setcookie('list', TaskProvider::addTask($_POST['oneMore'], $_COOKIE['list']));
+        $list = TaskProvider::addTask($_POST['oneMore'], $_COOKIE['list']);
     }
 
-    if (!is_null($list)) $list = explode(',', $_COOKIE['list']);
+    if (!is_null($list)) {
+        $list = (count(explode(',', $list)) > 1 ? 
+        explode(',', $list) : 
+        str_split($list, strlen($list))
+        );
+    }
     if (isset($_GET['todo'])) { 
         unset($list[$_GET['todo']]);
         setcookie('list',implode(',', $list));
         header('Location:?controller=todo');
     }
+
     $tasks = TaskProvider::getUndoneList($list);
     
     require_once 'view/todo.php';
